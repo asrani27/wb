9089da16d5cb18b59d43c34a838323f9e766b9f8
asrani <asrani.27@gmail.com>
2024-08-28 16:10:59 +0800

    f

--- resources/views/welcome2.blade.php
+++ resources/views/welcome2.blade.php
@@ -347,10 +347,10 @@
         <div class="col-lg-12">
             <br/>
             statistik pengunjung
-            <!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
-            <!-- Histats.com  START  (aync)-->
-            <script type="text/javascript">var _Hasync= _Hasync|| [];
-            _Hasync.push(['Histats.start', '1,4891314,4,424,112,75,00011111']);
+             <!-- Histats.com  (div with counter) --><div id="histats_counter"></div>
+<!-- Histats.com  START  (aync)-->
+        <script type="text/javascript">var _Hasync= _Hasync|| [];
+            _Hasync.push(['Histats.start', '1,4891316,4,424,112,75,00011111']);
             _Hasync.push(['Histats.fasi', '1']);
             _Hasync.push(['Histats.track_hits', '']);
             (function() {
@@ -358,8 +358,8 @@
             hs.src = ('//s10.histats.com/js15_as.js');
             (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
             })();</script>
-            <noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4891314&101" alt="" border="0"></a></noscript>
-
+            <noscript><a href="/" target="_blank"><img  src="//sstatic1.histats.com/0.gif?4891316&101" alt="" border="0"></a></noscript>
+            <!-- Histats.com  END  -->
         </div>
     </div>
     <footer id="footer" class="footer-area">
